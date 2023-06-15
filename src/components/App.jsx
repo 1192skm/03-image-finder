e2fcdb4709d823f467ee08c826b1151e1aca9342
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-06-15 19:13:29 +0300

    Refactor button loadMore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ export class App extends Component {
 
   async componentDidUpdate(_, prevState) {
     const { listName, page } = this.state;
-    if (prevState.listName !== this.state.listName) {
+    if (prevState.listName !== listName || prevState.page !== page) {
       this.abortCtrl = new AbortController();
       try {
         this.setState({ isLoading: true, error: null });
@@ -27,7 +27,10 @@ export class App extends Component {
           signal: this.abortCtrl.signal,
         });
         if (images.hits.length) {
-          this.setState({ list: images.hits, totalHits: images.totalHits });
+          this.setState(prevState => ({
+            list: [...prevState.list, ...images.hits],
+            totalHits: images.totalHits,
+          }));
         } else {
           this.setState({
             error: `Зображення по запиту ${listName} не знайдені!`,
@@ -41,30 +44,35 @@ export class App extends Component {
     }
   }
 
-  handleBtnMoreClick = async () => {
-    const { listName, page } = this.state;
-    this.abortCtrl = new AbortController();
-    try {
-      this.setState({
-        isLoading: true,
-        error: null,
-      });
-      const images = await getImages(listName, page +1, {
-        signal: this.abortCtrl.signal,
-      });
-      this.setState(prevState => ({
-        list: [...prevState.list, ...images.hits],
-        page: prevState.page +1,
-      }));
-    } catch (error) {
-      this.setState({ error: error.message });
-    } finally {
-      this.setState({ isLoading: false });
-    }
+  handleBtnMoreClick = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
+  // handleBtnMoreClick = async () => {
+  //   const { listName, page } = this.state;
+  //   this.abortCtrl = new AbortController();
+  //   try {
+  //     this.setState({
+  //       isLoading: true,
+  //       error: null,
+  //     });
+  //     const images = await getImages(listName, page +1, {
+  //       signal: this.abortCtrl.signal,
+  //     });
+  //     this.setState(prevState => ({
+  //       list: [...prevState.list, ...images.hits],
+  //       page: prevState.page +1,
+  //     }));
+  //   } catch (error) {
+  //     this.setState({ error: error.message });
+  //   } finally {
+  //     this.setState({ isLoading: false });
+  //   }
+  // };
 
   handleListNameSubmit = listName => {
-    this.setState({ listName, list: [], page: 1 });
+    this.setState({ list: [], listName, page: 1 });
   };
 
   render() {
@@ -82,9 +90,12 @@ export class App extends Component {
           </div>
         )}
         <ImageGallery list={list} />
-        {!isLoading && totalHits > 12 && !error && (
-          <BtnLoadMore onClick={this.handleBtnMoreClick} />
-        )}
+        {!isLoading &&
+          totalHits > 12 &&
+          !error &&
+          list.length !== totalHits && (
+            <BtnLoadMore onClick={this.handleBtnMoreClick} />
+          )}
       </div>
     );
   }
